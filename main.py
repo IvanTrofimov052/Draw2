1febaa0ff52edeb840284ca0e1981139878f83dd
IvanTrofimov <ivantrofimov052@gmail.com>
2019-10-11 18:23:57 +0300

    case#1

---
+++ main.py
@@ -0,0 +1,9 @@
+def Square(x,y,size,color,angel):
+    #Do(Гоша)
+    print()
+def Triangle(x,y,size,color,angel):
+    #Do(Егор)
+    print()
+def Parallelogram(x,y,size,color,angel):
+    # Do(Я)
+    print ()
